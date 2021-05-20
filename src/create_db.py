5fc58cb49263cfbe3e966702a0048268920ce712
Qiana Yang <qyang1997@outlook.com>
2021-05-20 00:23:07 -0500

    delete add_songs.py; add flask data ingestion code

--- src/create_db.py
+++ src/create_db.py
@@ -1,8 +1,8 @@
-import argparse
-
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
 from config.flaskconfig import logging, sql_uri
 
@@ -55,3 +55,67 @@ def create_new_db(eng_str=sql_uri):
     except sqlalchemy.exc.OperationalError:
         # Checking for correct credentials
         logger.error(f"create_db: Access to {eng_str} denied! Please enter correct credentials or check your VPN")
+
+
+class SurveyManager:
+
+    def __init__(self, app=None, engine_string=sql_uri):
+        """
+        Args:
+            app: Flask - Flask app
+            engine_string: str - Engine string
+        """
+        if app:
+            self.db = SQLAlchemy(app)
+            self.session = self.db.session
+        elif engine_string:
+            engine = sqlalchemy.create_engine(engine_string)
+            Session = sessionmaker(bind=engine)
+            self.session = Session()
+        else:
+            raise ValueError("Need either an engine string or a Flask app to initialize")
+
+    def close(self):
+        """Closes session"""
+        self.session.close()
+
+    def add_user_survey(self, user: str, question: str, answer: int):
+        """Add user survey record to existing database.
+
+        Args:
+            user: str - user id
+            question: str - question id
+            answer: int - answer
+
+        Returns: None
+        """
+
+        session = self.session
+        survey_record = Survey(user=user, question=question, answer=answer)
+        session.add(survey_record)
+        session.commit()
+        logger.info(f'User {user}: answer to {question} added to database.')
+
+    def add_user_metadata(self, user: str, age: int, gender: int, accuracy: int,
+                          country: str, source: int, elapsed: int):
+        """Add user metadata record to existing database.
+
+        Args:
+            user: str - user ID
+            age: int - user age
+            gender: int - user gender, one-hot encoded
+            accuracy: int - user self-identified survey result accuracy
+            country: str - user country
+            source: int - survey platform source
+            elapsed: int - time elapsed
+
+        Returns: None
+        """
+
+        session = self.session
+        metadata_record = Metadata(user=user, age=age, gender=gender,
+                                   accuracy=accuracy, country=country,
+                                   source=source, elapsed=elapsed)
+        session.add(metadata_record)
+        session.commit()
+        logger.info(f'User {user}: metadata added to database.')
