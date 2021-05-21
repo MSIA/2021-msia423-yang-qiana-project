04e636ffb28c8706331298076045f8b0c1721146
Qiana Yang <qyang1997@outlook.com>
2021-05-21 00:46:24 -0500

    update modeling pipeline and database schema

--- src/create_db.py
+++ src/create_db.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,34 +10,31 @@ Base = declarative_base()
 logger = logging.getLogger('create_db')
 
 
-class Survey(Base):
+class UserData(Base):
     """Create a data model for the database to be set up for user survey"""
 
-    __tablename__ = 'survey'
+    __tablename__ = 'user_data'
 
     user = Column(Integer, primary_key=True, nullable=False)
-    question = Column(String(10), primary_key=True, nullable=False)
-    answer = Column(Integer, unique=False, nullable=True)
-
-    def __repr__(self):
-        return f'<Survey user {self.user} question {self.question}'
-
-
-class Metadata(Base):
-    """Create a data model for the database to be set up for user metadata"""
-
-    __tablename__ = 'metadata'
-
-    user = Column(Integer, primary_key=True, nullable=False)
+    factor1 = Column(Float, unique=False, nullable=True)
+    factor2 = Column(Float, unique=False, nullable=True)
+    factor3 = Column(Float, unique=False, nullable=True)
+    factor4 = Column(Float, unique=False, nullable=True)
+    factor5 = Column(Float, unique=False, nullable=True)
+    factor6 = Column(Float, unique=False, nullable=True)
+    factor7 = Column(Float, unique=False, nullable=True)
+    factor8 = Column(Float, unique=False, nullable=True)
+    factor9 = Column(Float, unique=False, nullable=True)
+    factor10 = Column(Float, unique=False, nullable=True)
+    factor11 = Column(Float, unique=False, nullable=True)
+    factor12 = Column(Float, unique=False, nullable=True)
+    cluster = Column(Integer, unique=False, nullable=False)
     age = Column(Integer, unique=False, nullable=True)
     gender = Column(Integer, unique=False, nullable=True)
-    accuracy = Column(Integer, unique=False, nullable=True)
     country = Column(String(10), unique=False, nullable=True)
-    source = Column(Integer, unique=False, nullable=True)
-    elapsed = Column(Integer, unique=False, nullable=True)
 
     def __repr__(self):
-        return f'<Metadata user {self.user}'
+        return f'<Survey user {self.user} assigned to cluster {self.cluster}.'
 
 
 def create_new_db(eng_str=sql_uri):
@@ -79,55 +76,25 @@ class SurveyManager:
         """Closes session"""
         self.session.close()
 
-    def add_user_survey(self, user: str, question: str, answer: int):
+    def add_user_record(self, user: str, factors: dict, cluster: int, age: int, gender: int, country: str):
         """Add user survey record to existing database.
 
         Args:
-            user: str - user id
-            question: str - question id
-            answer: int - answer
-
-        Returns: None
-        """
-
-        session = self.session
-        survey_record = Survey(user=user, question=question, answer=answer)
-        session.add(survey_record)
-        session.commit()
-        logger.info(f'User {user}: answer to {question} added to database.')
-
-    def add_user_metadata(self, user: str, age: int, gender: int, accuracy: int,
-                          country: str, source: int, elapsed: int):
-        """Add user metadata record to existing database.
-
-        Args:
-            user: str - user ID
+            user: int - user id
+            factors: dict - dict of floats corresponding to latent variables 1 to 12
+            cluster: int - cluster assignment for user
             age: int - user age
-            gender: int - user gender, one-hot encoded
-            accuracy: int - user self-identified survey result accuracy
+            gender: int - user gender
             country: str - user country
-            source: int - survey platform source
-            elapsed: int - time elapsed
 
         Returns: None
         """
 
         session = self.session
-        metadata_record = Metadata(user=user, age=age, gender=gender,
-                                   accuracy=accuracy, country=country,
-                                   source=source, elapsed=elapsed)
-        session.add(metadata_record)
+        user_record = UserData(user=user, **factors, cluster=cluster, age=age, gender=gender, country=country)
+        session.add(user_record)
         session.commit()
-        logger.info(f'User {user}: metadata added to database.')
-
-    def add_initial_static_data(self):
-        """Add initial raw data into database."""
-        # read from s3
-
-        # split dataframe into two dataframes in the format of the tables
-
-        # ingest
-        session = self.session
+        logger.info(f"User {user}'s record added to database.")
 
     def clear_table(self, table):
         """Clear table in case things go wrong in the data ingestion process."""
